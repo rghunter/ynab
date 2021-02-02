391ac2c0f1c05960647f54423630a9fd2eecf67d
Ryan Hunter <ryan.g.hunter@gmail.com>
2021-02-02 10:46:54 -0500

    remove debug line

--- custom_components/ynab/__init__.py
+++ custom_components/ynab/__init__.py
@@ -119,7 +119,6 @@ class ynabData:
                 _LOGGER.debug("this_month: %s", this_month)
                 _LOGGER.debug("last month: %s", last_month)
                 if m.month == this_month:
-                    _LOGGER.debug("This Month: %s", m.month)
                     # Get age of money
                     self.hass.data[DOMAIN_DATA]["age_of_money"] = (
                         m.age_of_money
